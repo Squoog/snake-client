95276586f20fbd83b16907300ac06fb12c67b752
Ryan Zhen <rjhzhen@gmail.com>
2022-10-26 15:07:19 -0400

    refactored; connection requirements now imported

--- play.js
+++ play.js
@@ -1,14 +1,6 @@
-const net = require("net");
+const net = require('net');
+const connect = require('./client.js');
 
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
-  });
-
-  conn.setEncoding("utf8");
-  return conn;
-}
 
 console.log("Connecting ...");
 connect();
