b92fce0c123f8e26e1d8896d15a6a21674505fa8
Ryan Zhen <rjhzhen@gmail.com>
2022-10-26 17:36:13 -0400

    updated to display name

--- client.js
+++ client.js
@@ -1,13 +1,15 @@
 const net = require('net');
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "165.227.47.243",
-    port: 50541
+    port: 50541,
+    name: 'boj'
   });
 
   conn.setEncoding("utf8");
-  return conn;
-}
+  conn.write("Name: Bob");
 
+  return conn;
+};
 module.exports = connect;
