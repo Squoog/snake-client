279c6e12e09a542c39b668fe1f015d1694c5cd99
Ryan Zhen <rjhzhen@gmail.com>
2022-10-26 20:35:32 -0400

    added key input

--- play.js
+++ play.js
@@ -4,3 +4,27 @@ const connect = require('./client.js');
 
 console.log("Connecting ...");
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", key => {
+    handleUserInput(key);
+  });
+
+  return stdin;
+};
+setupInput();
+
+const handleUserInput = function (key) {
+  process.stdout.write(key);
+
+  if (key === '\u0003') {
+    process.stdout.write("Exiting snek game")
+    process.exit();
+  }
+
+};
